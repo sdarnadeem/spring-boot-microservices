bcbd1ad74de7e08f690abfa70cf09094cd9aa23e
sdarnadeem <sdarnadeem@gmail.com>
2023-02-15 14:58:09 +0530

    implement user controller

--- user-service/src/main/java/dev/nasyxnadeem/userservice/controllers/UserController.java
+++ user-service/src/main/java/dev/nasyxnadeem/userservice/controllers/UserController.java
@@ -1,16 +1,41 @@
 package dev.nasyxnadeem.userservice.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import dev.nasyxnadeem.userservice.models.*;
+import dev.nasyxnadeem.userservice.services.UserServices;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
+    private UserServices userService;
+
     // CRAETE USER
+    @PostMapping("")
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        return null;
+        User newUser = this.userService.saveUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = this.userService.getUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable String userId) {
+        User user = this.userService.getUserById(userId);
+        return ResponseEntity.ok(user);
     }
 
 }
