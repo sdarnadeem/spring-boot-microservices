8856f62044d2b023277cb5bdfd9c89bcd70a9869
sdarnadeem <sdarnadeem@gmail.com>
2023-02-20 13:05:34 +0530

    modify global config file

--- config-server/src/main/java/dev/nasyxnadeem/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/dev/nasyxnadeem/configserver/ConfigServerApplication.java
@@ -2,7 +2,7 @@ package dev.nasyxnadeem.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.configserver.EnableConfigServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
