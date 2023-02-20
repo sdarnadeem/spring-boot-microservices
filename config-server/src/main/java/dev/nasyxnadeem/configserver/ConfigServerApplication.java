84b62b8a3f8c8e0a2023d8e78cbf1238c792eff0
sdarnadeem <sdarnadeem@gmail.com>
2023-02-20 12:51:20 +0530

    add global config files

--- config-server/src/main/java/dev/nasyxnadeem/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/dev/nasyxnadeem/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package dev.nasyxnadeem.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.configserver.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
